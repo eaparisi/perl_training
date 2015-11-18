f2973efc1fab6957d970fc73dbc5c6353ba19e2f
ericparisi <eric.parisi@globant.com>
2015-11-18 17:16:10 -0300

    Ex_1

---
+++ phase_1/ex_1.pl
@@ -0,0 +1,55 @@
+#!/usr/bin/perl
+
+# Detect no numeric
+# looks_like_number - http://perlmaven.com/automatic-value-conversion-or-casting-in-perl
+# o usar reg expr
+
+use v5.14;
+use PadWalker;
+use Scalar::Util qw(looks_like_number);
+
+my $responseResult = 0;	# Successful execution
+my $result1;
+my $result2;
+
+my @parameters = @ARGV;
+my @defParameters = [];
+
+for (my $counter = 0; $counter < 3; $counter++) {
+	my $param = $parameters[$counter];
+	if (!defined $param) {
+		$param = 0;
+	} elsif (!looks_like_number($param)) {
+		print STDERR "Error - bad input: value '$param' is not number\n";
+		$responseResult = 1;
+		next;
+	}
+	$defParameters[$counter] = $param;
+}
+
+if ($responseResult == 0) {
+
+	my $a = $defParameters[0];
+	my $b = $defParameters[1];
+	my $c = $defParameters[2];
+	
+	my $sqrRootArgument = $b*$b - 4*$a*$c;
+	
+	if ($sqrRootArgument >= 0) {
+		my $sqrRoot = sqrt ($sqrRootArgument);	
+		$result1 = (-$b + $sqrRoot) / 2*$a;
+		$result2 = (-$b - $sqrRoot) / 2*$a;
+	} else {
+		print STDERR "Error - no real solutions: SqrRoot argument not positive\n";
+		$responseResult = 1;
+	}
+}
+
+if ($responseResult == 0) {
+	#	print STDOUT "Result 1: $result1 / Result 2: $result2" .'\n';
+	print STDOUT "$result1 $result2\n";
+	exit 0;
+} else {
+	print STDERR "Execution with errors\n";
+	exit 1;
+}
